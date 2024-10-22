26aac3de46504301749efc7ef4f231e06d94e775
ajin-urolime <168396432+ajin-urolime@users.noreply.github.com>
2024-10-22 09:23:42 +0530

    Update main.tf

--- Ajin_Abraham/CICD_Pipeline_with_AKS_and_DevSecOps_Tools/Environment/module/acr/main.tf
+++ Ajin_Abraham/CICD_Pipeline_with_AKS_and_DevSecOps_Tools/Environment/module/acr/main.tf
@@ -1,12 +1,12 @@
-locals {
+#locals {
   # Store gepreplications in map where key is location.
   # This automatically sorts the map by location.
-  georeplications = {
-    for georeplication in var.georeplications : georeplication.location => georeplication
-  }
+  # georeplications = {
+  #  for georeplication in var.georeplications : georeplication.location => georeplication
+  #}
 
-  diagnostic_setting_metric_categories = ["AllMetrics"]
-}
+  #diagnostic_setting_metric_categories = ["AllMetrics"]
+#}
 
 #--------------------------------------------------------------------------
 # ACR Creation
@@ -19,23 +19,23 @@ resource "azurerm_container_registry" "this" {
   admin_enabled                 = var.admin_enabled
   public_network_access_enabled = var.public_network_access_enabled
    
-  dynamic "georeplications" {
-    for_each = local.georeplications
+ # dynamic "georeplications" {
+ #   for_each = local.georeplications
 
-    content {
-      location                = georeplications.value["location"]
-      zone_redundancy_enabled = georeplications.value["zone_redundancy_enabled"]
-    }
-  }
+ #   content {
+ #    location                = georeplications.value["location"]
+ #     zone_redundancy_enabled = georeplications.value["zone_redundancy_enabled"]
+ #   }
+ # }
 
     lifecycle {
     # Prevent accidental destroy of Container Registry.
     prevent_destroy = false
 
-    precondition {
-      condition     = var.sku == "Premium" ? length(var.georeplications) >= 0 : length(var.georeplications) == 0
-      error_message = "Geo-replications can only be configured if SKU is \"Premium\"."
-    }
-  }
-}
+ #   precondition {
+ #     condition     = var.sku == "Premium" ? length(var.georeplications) >= 0 : length(var.georeplications) == 0
+ #     error_message = "Geo-replications can only be configured if SKU is \"Premium\"."
+ #   }
+ # }
+#}
 
