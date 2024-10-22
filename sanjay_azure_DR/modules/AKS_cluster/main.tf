768af4acac794ce1883a9f6485582ba5dd45b8a4
sanjaypramod <sanjaypramod.p@urolime.com>
2024-10-22 15:01:27 +0530

    added

---
+++ sanjay_azure_DR/modules/AKS_cluster/main.tf
@@ -0,0 +1,48 @@
+data "azurerm_kubernetes_service_versions" "current" {
+  location = var.location
+  include_preview = false
+}
+
+#-----------------------------------------------------------------------------------
+# aks cluster creation
+#----------------------------------------------------------------------------------
+resource "azurerm_kubernetes_cluster" "aks-cluster" {
+  name                     = var.cluster_name                 
+  location                 = var.location
+  resource_group_name     =  var.resource_group_name
+#   dns_prefix               = "${var.resource_group_name}-cluster"
+  dns_prefix = "${replace(var.resource_group_name, "_", "-")}-cluster"
+
+  kubernetes_version       = data.azurerm_kubernetes_service_versions.current.latest_version
+   
+  default_node_pool {
+  name                = var.default_node_pool_name
+  vm_size             = var.default_node_pool_vm_size  
+  zones               = var.default_node_pool_zones    
+  enable_auto_scaling = var.enable_auto_scaling        
+  max_count           = var.max_count                  
+  min_count           = var.min_count                   
+  os_disk_size_gb     = var.os_disk_size_gb            
+  vnet_subnet_id      = var.aks_subnet_id
+  type                = var.node_pool_type               
+  node_labels      = {
+        "nodepool-type" = var.nodepool-type
+        "environment"   = var.environment
+        "nodepoolos"  =  var.nodepoolos
+         }
+  tags = {
+        "nodepool-type"   = var.nodepool-type 
+        "environment"     = var.environment 
+        "nodepoolos"       = var.nodepoolos 
+          } 
+   }
+    
+    network_profile {
+      network_plugin = var.network_plugin                     
+      load_balancer_sku = var.load_balancer_sku                                   
+    } 
+    private_cluster_enabled = true
+    identity {
+    type = "SystemAssigned"  # or "UserAssigned" for user-assigned identity
+  }       
+}
---
+++ sanjay_azure_DR/modules/Blob_storage/variables.tf
@@ -0,0 +1,23 @@
+variable "blob_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "azurerm_storage_container_name" {
+  type = string
+}
+
+variable "container_access_type" {
+  type = string
+}
