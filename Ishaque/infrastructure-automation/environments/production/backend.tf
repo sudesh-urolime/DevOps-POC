c90c0b7b74796fc614292aefe97675c9f431a107
ishaquem-urolime <ishaque.abdul@urolime.com>
2024-11-04 11:15:16 +0000

    updated the backend s3 bucket to mealie-terraform-state-bucket-1234

--- Ishaque/infrastructure-automation/environments/production/backend.tf
+++ Ishaque/infrastructure-automation/environments/production/backend.tf
@@ -2,7 +2,7 @@
 # S3 bucket and dynamodb table are created manually.
 terraform {
   backend "s3" {
-    bucket         = "mealie-terraform-state-bucket-123"
+    bucket         = "mealie-terraform-state-bucket-1234"
     key            = "mealie/terraform.tfstate"
     region         = "us-east-1"
     #dynamodb_table = "terraform-state-lock"
