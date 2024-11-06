fa6c083e9767878775520233376121d5129c3b06
ishaquem-urolime <ishaque.abdul@urolime.com>
2024-11-06 12:10:37 +0000

    added provider.tf and versions.tf files to the production directory

--- Ishaque/infrastructure-automation/environments/production/variables.tf
+++ Ishaque/infrastructure-automation/environments/production/variables.tf
@@ -1,6 +1,6 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
+#variable "aws_region" {
+#  description = "AWS Region"
+#  type        = string
+#  default     = "us-east-1"
+#}
 
---
+++ Ishaque/infrastructure-automation/environments/production/versions.tf
@@ -0,0 +1,11 @@
+
+# Set the AWS provider configurations
+terraform {
+  required_version = "<= 1.9.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.71.0"
+    }
+  }
+}
