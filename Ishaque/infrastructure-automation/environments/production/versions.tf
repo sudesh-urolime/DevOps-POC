fef9854dfc8729bb591e24a38eb0491f8412cef6
ishaquem-urolime <ishaque.abdul@urolime.com>
2024-11-06 12:26:12 +0000

    updated terraform version constraints

--- Ishaque/infrastructure-automation/environments/production/versions.tf
+++ Ishaque/infrastructure-automation/environments/production/versions.tf
@@ -1,7 +1,7 @@
 
 # Set the AWS provider configurations
 terraform {
-  required_version = "<= 1.9.7"
+  required_version = "<= 1.9.8"
   required_providers {
     aws = {
       source = "hashicorp/aws"
